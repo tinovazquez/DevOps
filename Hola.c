6b43cb6d8b229bdb2eb68650c38011778598b56e
tinovazquez <tinovazquez@gmail.com>
2021-05-17 13:29:34 -0500

    Primer archivo subido

---
+++ Hola.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Todo lo que se pone después de doble slash va a ser ignorado por el compilador, así que lo utilizaremos para hacer comentarios
+//Autor: Tino Vázquez
+//versión: 11 de mayo 2017
+int main()
+{
+	printf("Hola mundo, soy Tinovich \n");
+	return 0;
+}
